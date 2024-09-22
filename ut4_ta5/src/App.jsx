4c6a635d9393e23bd48ae35795d3b7e2ed29c3fe
MHC2005 <mhcedres@gmail.com>
2024-09-22 18:50:20 -0300

    cambios

---
+++ ut4_ta5/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/button';  
+
+const App = () => {
+  return (
+    <div>
+      <h1>Texto que aparece y desaparece</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
